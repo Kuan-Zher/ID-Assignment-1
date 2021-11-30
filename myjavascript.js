70be7d6daab2b8d39a487a3369330cdc78423a1f
kuanzher <s10223870@connect.np.edu.sg>
2021-12-01 00:18:21 +0800

    commit 4

---
+++ myjavascript.js
@@ -0,0 +1,13 @@
+
+function links() {
+    alert("You are being redirected to another site. Click OK to confirm.");
+}
+
+
+function hoverHistory(a) {
+    a.style.boxShadow = "15px 15px gray";
+}
+
+function notHoverHistory(b) {
+    b.style.boxShadow = "";
+}
