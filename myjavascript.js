7d5c588fb19d533e0bce6b75978c5d5501f539eb
kuanzher <s10223870@connect.np.edu.sg>
2021-12-02 09:43:39 +0800

    commit 9

--- myjavascript.js
+++ myjavascript.js
@@ -1,13 +1,25 @@
 
+/*Function to display an alert message when the user submits a form*/
 function submission() {
     alert("Submission Received. Click OK to close.");
 }
 
+/*Function to highlight the navigation tab when the mouse is hovering over it*/
+function hoverHighlight(c) {
+    c.style.backgroundColor = "dodgerblue";
+}
 
+/*Function to remove the highlight on the navigation tab when the mouse is not hovering over it*/
+function notHoverHighlight(d) {
+    d.style.backgroundColor = "burlywood"
+}
+
+/*Function to create a shadow on the box when the mouse is hovering over it*/
 function hoverHistory(a) {
     a.style.boxShadow = "15px 15px gray";
 }
 
+/*Function to remove the shadow on the box when the mouse is not hovering over it*/
 function notHoverHistory(b) {
     b.style.boxShadow = "";
 }
